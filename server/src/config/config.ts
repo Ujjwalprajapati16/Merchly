b687ef901a8c6e2aebe8f98393430c414fe66689
Ujjwal Prajapati <ujjwalprajapati154@gmail.com>
2025-10-01 18:15:39 +0530

    env variables configuration

---
+++ server/src/config/config.ts
@@ -0,0 +1,9 @@
+const _config = {
+    port: process.env.PORT || 3000
+}
+
+const config = {
+    ..._config
+}
+
+export default config;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import app from "./app.ts";
+import config from "./config/config.ts";
 
-app.listen(3000, () => {
+app.listen(config.port, () => {
     console.log(`Server running at http://localhost:3000`);
 });
