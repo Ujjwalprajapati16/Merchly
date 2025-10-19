2c2446b03649a6f12bfb9185e6cc2c7b4486c715
Ujjwal Prajapati <ujjwalprajapati154@gmail.com>
2025-10-19 13:11:29 +0530

    feat: add user login

--- server/src/controllers/auth-controller.ts
+++ server/src/controllers/auth-controller.ts
@@ -1,5 +1,5 @@
 import type { NextFunction, Request, Response } from "express";
-import { createUser } from "../services/auth-services.ts";
+import { createUser, loginUserService } from "../services/auth-services.ts";
 import { generateJwt } from "../middlewares/JwtHandler.ts";
 import { BadRequest } from "../middlewares/ErrorHandler.ts";
 
@@ -24,3 +24,26 @@ export const registerUser = async (req : Request, res : Response, next : NextFun
         next(error);
     }
 };
+
+export const loginUser = async (req : Request, res : Response, next : NextFunction) => {
+    const { email, password } = req.body;
+
+    if(!email || !password ) {
+        throw new BadRequest("Missing required fields");
+    }
+
+    try {
+        const user = await loginUserService(email, password);
+
+        const token = generateJwt(user);
+
+        return res.status(200).json({ message: "User logged in successfully",token, user : {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role
+        } });
+    } catch (error) {
+        next(error);
+    }
+}
--- server/src/services/auth-services.ts
+++ server/src/services/auth-services.ts
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt';
-import type { RegisterUser } from '../types/User-types.ts';
-import { createNewUser } from '../repositories/user-repo.ts';
-import { APIError } from '../middlewares/ErrorHandler.ts';
+import type { RegisterUser, User } from '../types/User-types.ts';
+import { createNewUser, getUserByEmail } from '../repositories/user-repo.ts';
+import { APIError, NotFound, Unauthorized } from '../middlewares/ErrorHandler.ts';
 
 export const createUser = async (
   name: string,
@@ -22,6 +22,22 @@ export const createUser = async (
 
   if(!user) {
     throw new APIError(500, "User already exists");
+  }
+
+  return user;
+};
+
+export const loginUserService = async (email: string, password: string) => {
+  const user : User | null = await getUserByEmail(email);
+
+  if(!user) {
+    throw new NotFound("User not found");
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if(!isPasswordValid) {
+    throw new Unauthorized("Invalid credentials");
   }
 
   return user;
